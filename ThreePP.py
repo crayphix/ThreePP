e0d4ba4e1c8d9a257d2de5c6b5f7c38d8e6edddd
bspeelman <bryspeelm@gmail.com>
2018-09-07 22:04:37 -0400

    Prototype for host names

--- ThreePP.py
+++ ThreePP.py
@@ -1,39 +1,36 @@
 """
-Libraries needed to be installed: 
+Libraries needed to be installed:
 'pip install cymruwhois', 'pip install pythonwhois'
 """
 from cymruwhois import Client
 import dns.resolver
 import pythonwhois
 
-"""
-Function findDNS
-Program accepts file filled with hostnames then runs a dns lookup using
-dns.resolver
-Outputs table of hostnames with relevant ip addresses
-"""
+foo = 0
+
 # create a new instance named 'myResolver'
 myResolver = dns.resolver.Resolver()
 
-# Lookup the record(s) for amazon.com
-myAnswers = myResolver.query("npr.org")
+# create client object
+c = Client()
 
+#initialize domanin variable
+host = 'spotify.com'
+
+# Lookup the record(s) for amazon.com
+myAnswers = myResolver.query(host)
+
+print('\n' + host + '\n')
 for rdata in myAnswers:  # for each response
     print(rdata)  # print the data
-
-
-# query IP for owner using cymruwhois
-ip =('216.35.221.76')  # Assign IP
-c = Client()  # Create Client object
-r = c.lookup(ip)  # Assign lookup data to variable
-print(r.owner)  #  print owner
-"""
-Use lookupmany() to breeze through multiple ip addresses do not loop lookup()
-ex: for r in c.lookupmany([ip, ip_ms]):
-        print r.owner
-"""
+    print('Owner ---->')
+    foo = rdata
+    r = c.lookup(foo)  # Assign lookup data to variable
+    print(r.owner)  # print owner
+    print('-' * 70 + '\n')
 
 # get information from Arin using hostname
-domain = pythonwhois.get_whois('npr.org')
+print(host + ' Registrar ------>')
+domain = pythonwhois.get_whois(host)
 print(domain['registrar'])
-
+print
