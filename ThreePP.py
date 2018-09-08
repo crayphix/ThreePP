ca22a199b480d270685e2f8449d8f1dac48bb2ba
Bryan Speelman <Bryspeelm@gmail.com>
2018-09-08 16:10:33 -0400

    Testing ip address input

--- ThreePP.py
+++ ThreePP.py
@@ -9,31 +9,41 @@ import sys
 
 foo = 0
 
-#initialize domanin variable  
-host = sys.argv[1]
-
-
-# create a new instance named 'myResolver'
-myResolver = dns.resolver.Resolver()
-
 # create client object
 c = Client()
 
-# Lookup the record(s) for amazon.com
-myAnswers = myResolver.query(host)
+#initialize domanin variable with arg from cmd line  
+host = sys.argv[1]
 
-print('\n' + host + '\n')
-for rdata in myAnswers:  # for each response
-    print(rdata)  # print the data
+if('.com' in host):
+    # create a new instance named 'myResolver'
+    myResolver = dns.resolver.Resolver()
+
+    # Lookup the record(s) for amazon.com
+    myAnswers = myResolver.query(host)
+
+    # loop output if multiple IP's
+    print('\n' + host + '\n')
+    for rdata in myAnswers:  # for each response
+        print(rdata)  # print the data
+        print('Owner ---->')
+        foo = rdata
+        r = c.lookup(foo)  # Assign lookup data to variable
+        print(r.owner)  # print owner
+        print('-' * 70 + '\n')
+
+    # get registrar from Arin using hostname
+    print(host + ' Registrar ------>')
+    domain = pythonwhois.get_whois(host)
+    print(domain['registrar'])
+    print
+
+
+else:
+    print('\n' + host)
     print('Owner ---->')
-    foo = rdata
-    r = c.lookup(foo)  # Assign lookup data to variable
+    r = c.lookup(host)  # Assign lookup data to variable
     print(r.owner)  # print owner
     print('-' * 70 + '\n')
 
-# get information from Arin using hostname
-print(host + ' Registrar ------>')
-domain = pythonwhois.get_whois(host)
-print(domain['registrar'])
-print
 
