1d5540a7e71333d078b094367742e7214ff8370b
bspeelman <bryspeelm@gmail.com>
2018-10-03 09:11:25 -0400

    new test data

--- ThreePP.py
+++ ThreePP.py
@@ -18,31 +18,33 @@ import dns.resolver
 import pythonwhois
 import sys
 import socket
+import ansible
 
 def main(): 
     # Use cmd line arg flag to determine what type of lookup to perform
     if(sys.argv[1] == '-f'):
-        inFile(sys.argv[2])
+        inFile(sys.argv[2]) # call inFile method with 2nd cmd line arg
     elif(sys.argv[1] == '-s'):
-        singleQuery(sys.argv[2])
+        singleQuery(sys.argv[2]) # call singleQuery method with 2nd cmd line arg
     else:
         print('*' * 70 +"\nPlease use -f to indicate file or -s to indicate single query."
-            "\nexample: -f test.txt or -s google.com")
+            "\nexample: -f test.txt or -s google.com") # display flag info if invalid input
 
 # function that takes host domain name and prints IP addresses w/ ownership
 # and registrar 
 def getHostinfo(hostname):
     # create client object
     c = Client()
-
-    # Lookup the record(s) host amazon.com
-    myAnswers = dns.resolver.query(hostname) 
-  
+    
+    # Lookup the record(s) host 
+    resolver = dns.resolver.Resolver();
+    myAnswers = resolver.query(hostname, 'A') 
+ 
     # loop output if multiple IP's
     print('\n' + hostname + '\n')
     for rdata in myAnswers:  # for each response
-        print(c.lookup(rdata))  # print data
-    
+        print(c.lookup(rdata.address))  # print data
+         
     # get registrar from Arin using hostname
     print('\n' + hostname + ' Registrar')
     domain = pythonwhois.get_whois(hostname)
